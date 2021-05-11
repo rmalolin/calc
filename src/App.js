ea064d419f16035a95c5aa4a34d979f41f1ea61a
rmalolin <steammushroom@gmail.com>
2021-05-11 14:19:02 +0300

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import NumberFormat from 'react-number-format';
 
 
 function App() {
@@ -10,32 +11,89 @@ function App() {
   const [total, setTotal] = useState(false)
 
   const inputNum = e => {
-
+    if (curState.includes(".") && e.target.innerText === ".") return
+    if (total) {
+      setPreState("")
+    }
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText)
+    setTotal(false)
   }
 
-  const operatorType = e => {
+  useEffect(() => {
+    setInput(curState)
+  }, [curState])
 
+  useEffect(() => {
+    setInput("0")
+  }, [])
+
+  const operatorType = (e) => {
+    setTotal(false)
+    setOperator(e.target.innerText)
+    if (curState === "") return
+    if (preState !== "") {
+      equals()
+    } else {
+      setPreState(curState)
+      setCurState("")
+    }
   }
 
-  const equals = e => {
-
+  const equals = (e) => {
+    if (e?.target.innerText === "=")
+      setTotal(true)
+    let cal
+    switch (operator) {
+      case "/":
+        cal = String(parseFloat(preState) / parseFloat(curState))
+        break;
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(curState))
+        break;
+      case "-":
+        cal = String(parseFloat(preState) - parseFloat(curState))
+        break;
+      case "X":
+        cal = String(parseFloat(preState) * parseFloat(curState))
+        break;
+      default:
+        return
+    }
+    setInput("")
+    setPreState(cal)
+    setCurState("")
   }
 
   const minusPlus = () => {
-
+    if (curState.charAt(0) === "-") {
+      setCurState(curState.substring(1))
+    } else {
+      setCurState("-" + curState)
+    }
   }
 
   const percent = () => {
-
+    preState
+      ? setCurState(String(parseFloat(curState) / 100 * preState))
+      : setCurState(String(parseFloat(curState) / 100))
   }
 
   const reset = () => {
-
+    setPreState("")
+    setCurState("")
+    setInput("0 ")
   }
   return (
     <div className="container">
       <div className="wrapper">
-        <div className="screen"></div>
+        <div className="screen">
+          {input !== "" || input === "0"
+            ? <NumberFormat value={input} displayType={"text"} thousandSeparator={true} />
+            : <NumberFormat value={preState} displayType="text" thousandSeparator={true} />
+          }
+        </div>
         <div className="btn light-gray" onClick={reset}>AC</div>
         <div className="btn light-gray" onClick={percent}>%</div>
         <div className="btn light-gray" onClick={minusPlus}>+/-</div>
